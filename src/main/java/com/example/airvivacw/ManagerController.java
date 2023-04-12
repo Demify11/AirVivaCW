ad89ef9f84b6699adab309395d46302afe22e354
Demify11 <Demetrious3000@gmail.com>
2023-04-12 13:54:44 +0100

    Workable manager interface without flexiable or reports push. Manage blank Functionality withdropdown, change discount rate for customer.

--- src/main/java/com/example/airvivacw/ManagerController.java
+++ src/main/java/com/example/airvivacw/ManagerController.java
@@ -1,14 +1,122 @@
 package com.example.airvivacw;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
 
 public class ManagerController {
+
+    @FXML
+    private TextField searchCustomerField;
+    @FXML
+    private TextField fixedDiscountField;
+    @FXML
+    private Label updateDiscountLabel;
+    @FXML
+    private ComboBox usersDropDown;
+    @FXML
+    private TextField enterBlankAssignField;
+
+
+    @FXML
+    private TableView<ViewBlanks> viewblankstable;
+    @FXML
+    private TableColumn<ViewBlanks, Integer> numbers;
+    @FXML
+    private TableColumn<ViewBlanks, Integer> ids;
+    @FXML
+    private TableColumn<ViewBlanks, String> owners;
+    @FXML
+    private TableColumn<ViewBlanks, String> types;
+
+    ObservableList<ViewBlanks> blanksList = FXCollections.observableArrayList();
+
+    DatabaseConnection connectNow = new DatabaseConnection();
+    Connection connectDB = connectNow.getConnection();
 
     private SceneController sceneController = new SceneController();
 
     public void logoutButtonOnAction(ActionEvent e) throws IOException {
         sceneController.switchToLoginPage(e);
     }
+
+    public void updateFixedDiscount(ActionEvent e) throws SQLException {
+        if (!searchCustomerField.getText().isBlank()) {
+            Statement statement = connectDB.createStatement();
+            String findDiscount = "UPDATE AVcustomer SET fixed_discount ='" + fixedDiscountField.getText() + "' WHERE email_address ='" + searchCustomerField.getText() + "'";
+            int queryResult = statement.executeUpdate(findDiscount);
+            if (queryResult > 0) {
+                updateDiscountLabel.setText("You Updated The Discount Rate");
+            }
+        }
+    }
+
+
+
+    public void loadBlanksOnAction(ActionEvent e) throws SQLException {
+
+        ObservableList<ViewBlanks> listblank = FXCollections.observableArrayList();
+
+        Statement statement = connectDB.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM AVblank");
+
+        ids.setCellValueFactory(new PropertyValueFactory<ViewBlanks,Integer>("ids"));
+        numbers.setCellValueFactory(new PropertyValueFactory<ViewBlanks,Integer>("numbers"));
+        types.setCellValueFactory(new PropertyValueFactory<ViewBlanks,String>("types"));
+        owners.setCellValueFactory(new PropertyValueFactory<ViewBlanks,String>("blankowners"));
+
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String number = resultSet.getString("number");
+            String type = resultSet.getString("type");
+            String blankowner = resultSet.getString("blankowner");
+
+            ViewBlanks viewblanks = new ViewBlanks(id, number, type, blankowner);
+            listblank.add(viewblanks);
+        }
+
+        viewblankstable.setItems(listblank);
+
+        Statement statement1 = connectDB.createStatement();
+        ResultSet resultSet1 = statement1.executeQuery("SELECT name FROM AVuser");
+        ObservableList data = FXCollections.observableArrayList();
+        while (resultSet1.next()){
+            data.add(new String(resultSet1.getString(1)));
+        }
+        usersDropDown.setItems(data);
+
+
+
+    }
+
+
+
+
+    public void assignBlankOnAction(ActionEvent e) throws SQLException {
+        Statement statement2 = connectDB.createStatement();
+        String assignblankfunc = "UPDATE AVblank SET blankowner ='" + usersDropDown.getSelectionModel().getSelectedItem().toString() + "' WHERE number ='" + enterBlankAssignField.getText() + "'";
+        int queryResult2 = statement2.executeUpdate(assignblankfunc);
+        if (queryResult2 > 0) {
+            updateDiscountLabel.setText("You Updated The Discount Rate");
+        }
+
+    }
+
+    public void clickedColumn(ActionEvent e){
+
+    }
+
+
 }
---
+++ src/main/java/com/example/airvivacw/ViewBlanks.java
@@ -0,0 +1,37 @@
+package com.example.airvivacw;
+
+public class ViewBlanks {
+    private int ids;
+    private String numbers;
+    private String types;
+    private String blankowners;
+
+    public ViewBlanks(int ids, String numbers, String types, String blankowners) {
+        this.ids = ids;
+        this.numbers = numbers;
+        this.types = types;
+        this.blankowners = blankowners;
+    }
+
+    public int getIds() {
+        return ids;
+    }
+
+    public String getNumbers() {
+        return numbers;
+    }
+
+    public String getTypes() {
+        return types;
+    }
+
+    public String getBlankowners() {
+        return blankowners;
+    }
+
+
+
+
+
+}
+
